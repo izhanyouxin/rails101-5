b691d30da37efa0de344b73af4f3466786019d05
zhanyouxin <zhanyouxin@gmail.com>
2016-12-18 10:58:15 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,7 +21,7 @@ def create
   @group.user = current_user
 
   if @group.save
-
+  current_user.join!(@group)
   redirect_to groups_path
 else
   render :new
@@ -48,6 +48,7 @@ current_user.join!(@group)
 flash[:notice] = "加入本讨论版成功！"
 else
   flash[:warning] = "你已经是本讨论版的成员了！"
+end
 end
 
 def quit
